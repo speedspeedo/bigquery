25dd5931bb039e446469f22892fa2a040b87d1e9
speedspeedo <speedodeveloper1004@gmail.com>
2018-09-13 01:41:28 +0700

    Add ssl and Add settings

--- settings/setting.go
+++ settings/setting.go
@@ -4,20 +4,58 @@ import (
 	"os"
 	"log"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"fmt"
+	"encoding/json"
 )
 
+var environments = map[string]string{
+	"production":    "settings/prod.json",
+	"development": "settings/dev.json",
+}
+
+type Settings struct {
+	PrivateKeyPath     string
+	PublicKeyPath      string
+	JWTExpirationDelta int
+}
+
+var settings Settings = Settings{}
 var env = "development"
 
 func Init() {
 	env := os.Getenv("GO_ENV")
 	if env == "" {
-		log.Println("Run Application Mode Development")
+		log.Println("Run Application Mode " + env)
 		env = "development"
+	}
+	LoadSettingsEnv(env)
+}
+func LoadSettingsEnv(env string) {
+	conn, err := ioutil.ReadFile(environments[env])
+	if err != nil {
+		fmt.Println("Error while reading configuration file", err)
+	}
+	settings = Settings{}
+	jErr := json.Unmarshal(conn, &settings)
+	if jErr != nil {
+		fmt.Println("Error while parsing configuration file", jErr)
 	}
 }
 
 func GetEnvironment() string {
 	return env
+}
+
+func Get() Settings {
+	if &settings == nil {
+		Init()
+	}
+	return settings
+}
+
+func IsModeEnvironment() bool {
+	return env == "production"
 }
 
 func Cores() gin.HandlerFunc {
