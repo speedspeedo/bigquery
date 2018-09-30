2cc24ba8a6cb76435dc0ab499887e73f7967263a
speedspeedo <speedodeveloper1004@gmail.com>
2018-09-30 18:06:40 +0700

    SetMode Run Application

--- main.go
+++ main.go
@@ -4,11 +4,26 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/suriyajaboon/bigquery/settings"
 	"github.com/suriyajaboon/bigquery/routers"
+	"os"
+	"io"
 )
 
 func main()  {
 	settings.Init()
-	route := gin.Default()
+
+	// Log file
+	f, _ := os.Create("bigQuery.log")
+	gin.DefaultWriter = io.MultiWriter(f)
+
+	// Configuration Production Mode
+	gin.SetMode(gin.ReleaseMode)
+	route := gin.New()
+	route.Use(gin.Logger())
+	route.Use(gin.Recovery())
+
+	// Development Mode
+	//route := gin.Default()
+
 	route.Use(settings.Cores())
 	routers.InitRoutes(route)
 	route.Run( "localhost:8090")
