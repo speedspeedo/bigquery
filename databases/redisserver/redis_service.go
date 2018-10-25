8079e354ff451dd15397242e88b78698a55bcc45
speedspeedo <speedodeveloper1004@gmail.com>
2018-10-25 23:48:43 +0700

    Add Postgresql

--- databases/redisserver/redis_service.go
+++ databases/redisserver/redis_service.go
@@ -1,14 +1,27 @@
 package redisserver
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+)
 
-type IReDisServer interface {
-	RedisClient() (*redis.Client, error)
+func RedisNewClient(rn *RedisServer) IReDisServer {
+	return &RedisServer{rn.Host, rn.Port, rn.Password, rn.DB}
 }
 
-type RedisServer struct {
-	Host string
-	Port string
-	Password string
-	DB int
+func (rs *RedisServer) RedisClient() (*redis.Client, error) {
+	client, err := rs.connectToRedis()
+	if err != nil {
+		defer client.Close()
+		return nil, err
+	}
+	return client, nil
+}
+
+func (rs *RedisServer) connectToRedis() (*redis.Client, error) {
+	client := redis.NewClient(&redis.Options {
+		Addr: rs.Host + ":" + rs.Port,
+		Password: rs.Password,
+		DB: rs.DB,
+	})
+	return client, nil
 }
